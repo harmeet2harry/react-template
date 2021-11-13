244e825607fa10bf8a12767e85722622d406d9c5
harmeet2harry <harmeet2harry@gmail.com>
2021-11-13 11:06:31 +0530

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+// import "./styles.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Code</h1>
+      <h2>Start editing to see soe magic happen!</h2>
+    </div>
+  );
+}
